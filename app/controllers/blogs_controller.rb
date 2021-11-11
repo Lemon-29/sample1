08a1fd59234727f5053ba7abe361b564afbd6f5c
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-11 21:25:27 +0900

    set strong_params

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,4 +12,9 @@ class BlogsController < ApplicationController
     redirect_to new_blog_path
   end
 
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
 end
