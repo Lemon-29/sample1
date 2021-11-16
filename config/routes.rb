aee3e4128900826fc236db9f1387449833f72707
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-16 14:32:44 +0900

    set routing for comfirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # get 'blogs/index'
   # get '/blogs', to:'blogs#index'
-  resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
